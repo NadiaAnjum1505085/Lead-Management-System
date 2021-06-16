1598797ddcd5b25289fd8c05d09efb49e077b35f
nadiaanjum <nadia4anjum421@gmail.com>
2021-06-16 21:57:08 +0600

    Completed Migration

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Subordinate(models.Models):               #naming convention of table-> Capital first letter, Singular number
+class Subordinate(models.Model):               #naming convention of table-> Capital first letter, Singular number
     name=models.CharField(max_length=50)        # first column
     emali=models.EmailField(max_length=80,unique=True)
     designation=models.CharField(max_length=50,blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
-    
+
 
 
 
